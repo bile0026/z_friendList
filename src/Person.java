0eac7bd611913247fe13c3815ac11188a5b57cc9
bile0026 <zacharybiles@gmail.com>
2020-09-20 21:51:20 -0500

    working on unfriend and getfriendnames

--- src/Person.java
+++ src/Person.java
@@ -6,13 +6,12 @@ public class Person {
     public int getNumberOfFriends;
     public String Person;
     public int friendCount;
-    ArrayList<String> friendList;
+    ArrayList<Person> friendList;
 
     public Person(final String newName) {
         Name = newName;
         friendCount = 0;
         // friendList = "";
-        ArrayList<String> friendList = new ArrayList<String>();
     }
 
     public String getName(final String Name) {
@@ -23,19 +22,19 @@ public class Person {
     public String befriend(final Person p) {
         friendCount = friendCount + 1;
         // friendList.add(p.getName(Person));
-        friendList.add(p.Name);
+        friendList.add(p);
         return Person;
 
     }
 
-    public String unfriend(final Person p) {
+    public Person unfriend(final Person p) {
         friendCount = friendCount - 1;
         for (Person person : friendList) {
             if (person == p) {
                 friendList.remove(p);
             }
         }
-        friendList.remove(p);
+        return Person p;
     }
 
     private String replace(final String person2, final String string) {
@@ -43,7 +42,14 @@ public class Person {
     }
 
     public String getFriendNames() {
-        return friendList;
+        String friendNameList[];
+
+        for (Person friend : friendList) {
+            int i = 0;
+            friendNameList[i] = friend.Name;
+            i++;
+        }
+        return friendNameList.toString();
 
     }
 
